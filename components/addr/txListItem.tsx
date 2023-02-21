aaf1a77d3a7e51c299bfa0da9db8eaa8ffc27196
wonjongKim <kwjdnjs@gmail.com>
2023-02-21 13:14:28 +0900

    update tx list

--- components/addr/txListItem.tsx
+++ components/addr/txListItem.tsx
@@ -4,23 +4,44 @@ import Link from "next/link";
 export default function TxListItem ({ addr, txHash }) {
     const data = useFetch('tx', txHash, true);
     let vinValue = 0;
+    let vinAddrList: string[] = [];
     let voutValue = 0;
+    let voutAddrList: string[] = [];
+    let isWithdrawal = false;
+    let inOutValue = 0;
+
+    function getValue(addr, vinOut): any[] {
+        let value = 0;
+        let addrList: string[] = [];
+
+        for (const i in vinOut) {
+            if(addr == vinOut[i]['addresses']) value = vinOut[i]['value'];
+            else addrList.push(vinOut[i]['addresses']);
+        }
+
+        return [value, addrList];
+    };
 
     if (data) {
         const vin = data['data']['vin'];
-        for (const i in vin)
-            if (addr == vin[i]['addresses']) vinValue = vin[i]['value'];
+        [vinValue, vinAddrList] = getValue(addr, vin);
 
         const vout = data['data']['vout'];
-        for (const i in vout)
-            if (addr == vout[i]['addresses']) voutValue = vout[i]['value'];
+        [voutValue, voutAddrList] = getValue(addr, vout);
+
+        inOutValue = vinValue - voutValue
+        if (inOutValue > 0) isWithdrawal = true;
+        else inOutValue = -inOutValue;
     }
 
     return (
         <li>
             <Link href={`/tx/${txHash}`}>{txHash}</Link>
-            <div>vin: {vinValue}</div>
-            <div>vout: {voutValue}</div>
+            {
+                isWithdrawal
+                ? <div>출금: {inOutValue}<ul>{voutAddrList.map((addr, index) => (<li key={index}>{addr}</li>))}</ul></div>
+                : <div>입금: {inOutValue}<ul>{vinAddrList.map((addr, index) => (<li key={index}>{addr}</li>))}</ul></div>
+            }
         </li>
     )
 }
